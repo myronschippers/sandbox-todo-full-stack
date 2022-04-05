0cf5671f20e66a57b4d6445f8195d00e9c7c2350
Myron R Schippers Jr <dubtwee@gmail.com>
2022-04-05 11:17:42 -0500

    making a very simple GET route for say-something

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('say-something')
+  saySomething(): string {
+    return 'Is this thing on?';
+  }
 }
